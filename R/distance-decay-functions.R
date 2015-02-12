cf13ba0c4aece95eaa6297a60681fe704e03b6d7
Robinlovelace <rob00x@gmail.com>
2015-02-12 12:09:29 +0000

    Added example of distance decay function

--- R/distance-decay-functions.R
+++ R/distance-decay-functions.R
@@ -7,6 +7,12 @@
 #' params <- c(0, -0.002, -0.15, -3)
 #' (res <- log_cubic(d, c(0, -0.002, -0.15, -3)))
 #' plot(d, res)
+#'
+#' par_male_urb <- c(0.0001945, -0.002273, -0.1489583, -2.923221)
+#' par_female_urb <- c(-0.0017932, 0.0655261, -0.8396819, -2.892149)
+#' d = seq(0, 50, 0.1)
+#' plot(d, log_cubic(d, par = par_male_urb), ylim = c(0, 0.1)) # test plots
+#' lines(d, log_cubic(d, par = par_female_urb), ylim = c(0, 0.1)) # test plots
 log_cubic <- function(d, par){
   log_d <- log(d)
   log_p <- par[1] * d^3 + par[2] * d^2 + par[3] * d + par[4]
