4104d2c46b1c350fc7ad8fd5ba0d27bdabaf82a2
Robinlovelace <rob00x@gmail.com>
2015-05-08 15:25:03 +0100

    Typo correction and package help page

---
+++ R/stplanr-package.R
@@ -0,0 +1,11 @@
+#' The stplanr package
+#'
+#' The stplanr package provides functions to access and analyse data for transportation research, including origin-destination analysis, route allocation and modelling travel patterns.
+#'
+#' @name stplanr-package
+#' @aliases stplanr stplanr-package
+#' @docType package
+#' @author \email{rob00x@@gmail.com}
+#' @keywords package
+#' @seealso \url{https://github.com/Robinlovelace/stplanr}
+NULL
