401de452509d42b075e8e302dc0699c79a2917c4
robinlovelace <rob00x@gmail.com>
2015-10-18 16:24:52 +0100

    Update stplanr package documentation
    This is designed to fix this issue:
    https://github.com/ropensci/onboarding/issues/10
    "You need to import packages explicitly that you use."

--- R/stplanr-package.R
+++ R/stplanr-package.R
@@ -1,11 +1,27 @@
-#' The stplanr package
-#'
-#' The stplanr package provides functions to access and analyse data for transportation research, including origin-destination analysis, route allocation and modelling travel patterns.
+#' @title \strong{stplanr: Sustainable Transport Planning with R}
 #'
 #' @name stplanr-package
 #' @aliases stplanr stplanr-package
 #' @docType package
-#' @author \email{rob00x@@gmail.com}
+#' @author Robin Lovelace \email{rob00x@@gmail.com}
 #' @keywords package
 #' @seealso \url{https://github.com/Robinlovelace/stplanr}
+#' @description The stplanr package provides functions to access
+#' and analyse data for transportation research, including origin-destination analysis,
+#' route allocation and modelling travel patterns.
+#'
+#' @section Interesting functions:
+#' \itemize{
+#'  \item \code{\link{gOverline}} - Aggregate overlaying route lines and data intelligently
+#'  \item \code{\link{calc_catchment}} - Create a 'catchment area' to show the areas serving a destination
+#'  \item \code{\link{route_cyclestreet}} - Finds the fastest routes for cyclists between two places.
+#' }
+#'
+#' @import sp
+#' @import rgdal
+#' @import rgeos
+#' @importFrom openxls readWorkbook
+#' @importFrom ggmap geocode
+#' @importFrom jsonlite fromJSON
+#' @importFrom raster extent crop
 NULL
--- cran-comments.md
+++ cran-comments.md
@@ -1,21 +1,17 @@
 ## Test environments
-* local OS X install, R 3.1.2
-* ubuntu 12.04 (on travis-ci), R 3.1.2
-* win-builder (devel and release)
+* local OS X install, R 3.2.2
+* ubuntu 12.04 (on travis-ci), R 3.2.2
+* local windows 7 install
 
 ## R CMD check results
 There were no ERRORs or WARNINGs. 
 
-There was 1 NOTE:
+There were 2 NOTEs:
 
+* checking Rd line widths ... NOTE
+Rd file 'calc_catchment.Rd':
+  \usage lines wider than 90 characters:
+       projection = "+proj=aea +lat_1=90 +lat_2=-18.416667 +lat_0=0 +lon_0=10 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no ... [TRUNCATED]
+       
 * checking dependencies in R code ... NOTE
-  Namespace in Imports field not imported from: 'R6'
-
-  R6 is a build-time dependency.
-
-## Downstream dependencies
-I have also run R CMD check on downstream dependencies of httr 
-(https://github.com/wch/checkresults/blob/master/httr/r-release). All packages 
-that I could install passed except:
-
-* XYZ:...
+Package in Depends field not imported from: ‘sp’
