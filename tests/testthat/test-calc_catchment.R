3b2b368573cf9109cabaadf29bcd0abd9579beec
Richard Ellison <r.ellison@itls.usyd.edu.au>
2015-10-25 22:16:02 +1100

    Improvements to calc_catchment functions, addition of data to extdata folder and correction of examples. Also added testthat files.

---
+++ tests/testthat/test-calc_catchment.R
@@ -0,0 +1,20 @@
+context("Test the calc_catchment function")
+
+test_that(
+  desc = "calc_catchment returns a SpatialPolygonsDataFrame",
+  code = {
+    data_dir <- system.file("extdata", package = "stplanr")
+    unzip(file.path(data_dir, 'smallsa1.zip'))
+    unzip(file.path(data_dir, 'testcycleway.zip'))
+    sa1income <- readOGR(".","smallsa1")
+    testcycleway <- readOGR(".","testcycleway")
+    t1 <- calc_catchment(
+      polygonlayer = sa1income,
+      targetlayer = testcycleway,
+      calccols = c('Total'),
+      distance = 800,
+      projection = 'austalbers',
+      dissolve = TRUE
+    )
+    expect_is(t1, "SpatialPolygonsDataFrame")
+  })
