6cc0b1f728c1ad3c9ce032ad798e385ce9b594c5
Robin <rob00x@gmail.com>
2015-12-04 15:11:13 +0000

    Remove hardcoded data from stats19 funs

--- R/load-stats19.R
+++ R/load-stats19.R
@@ -81,7 +81,6 @@ read_stats19_ac <- function(data_dir = tempdir(), filename = "Accidents0514.csv"
 #' This is a helper function to format raw stats19 data
 #'
 #' @param ac Dataframe representing the raw Stats19 data read-in with \code{read_csv()}.
-#' @param wb List of dataframes containing data labels for stats19 data
 #'
 #' @export
 #' @examples
@@ -89,8 +88,6 @@ read_stats19_ac <- function(data_dir = tempdir(), filename = "Accidents0514.csv"
 #' ac <- format_stats19_ac(ac)
 #' }
 format_stats19_ac <- function(ac){
-
-  data("wb")
 
   ac$Accident_Severity <-
     factor(ac$Accident_Severity, labels = c("Fatal", "Serious", "Slight"))
@@ -183,35 +180,53 @@ read_stats19_ve <- function(data_dir = tempdir(), filename = "Vehicles0514.csv")
 #' This is a helper function to format raw stats19 data
 #'
 #' @param ve Dataframe representing the raw Stats19 data read-in with \code{read_csv()}.
-#' @param wb List of dataframes containing data labels for stats19 data
 #' @export
 #' @examples
 #' \dontrun{
 #' ve <- format_stats19_ve(ve)
 #' }
-format_stats19_ve <- function(ve, wb){
+format_stats19_ve <- function(ve){
 
-  data("wb")
-
-  tfact <-
-    wb$Vehicle.Type$label[ as.character(wb$Vehicle.Type$code) %in%
-                             levels(as.factor(ve$Vehicle_Type)) ]
-  ve$Vehicle_Type <- factor(ve$Vehicle_Type,
-                          labels=tfact[c(20, 1:19, 21)])
+ve$Vehicle_Type <- factor(ve$Vehicle_Type,
+                          labels = c("Goods vehicle - unknown weight", "Pedal cycle", "Motorcycle 50cc and under",
+                                     "Motorcycle 125cc and under", "Motorcycle over 125cc and up to 500cc",
+                                     "Motorcycle over 500cc", "Taxi/Private hire car", "Car", "Minibus (8 - 16 passenger seats)",
+                                     "Bus or coach (17 or more pass seats)", "Ridden horse", "Agricultural vehicle",
+                                     "Tram", "Van / Goods 3.5 tonnes mgw or under", "Goods over 3.5t. and under 7.5t",
+                                     "Goods 7.5 tonnes mgw and over", "Mobility scooter", "Electric motorcycle",
+                                     "Other vehicle", "Motorcycle - unknown cc", "Data missing or out of range"
+                          ))
   # summary(ve$Vehicle_Type)
   ve$Vehicle_Manoeuvre <-
-    factor(ve$Vehicle_Manoeuvre, labels = wb$Vehicle.Manoeuvre$label[c(19,1:18)])
+    factor(ve$Vehicle_Manoeuvre,
+           labels = c("Data missing or out of range", "Reversing", "Parked", "Waiting to go - held up",
+                      "Slowing or stopping", "Moving off", "U-turn", "Turning left",
+                      "Waiting to turn left", "Turning right", "Waiting to turn right",
+                      "Changing lane to left", "Changing lane to right", "Overtaking moving vehicle - offside",
+                      "Overtaking static vehicle - offside", "Overtaking - nearside",
+                      "Going ahead left-hand bend", "Going ahead right-hand bend",
+                      "Going ahead other"))
   # summary(ve$Vehicle_Manoeuvre)
   ve$Journey_Purpose_of_Driver <-
-    factor(ve$Journey_Purpose_of_Driver, labels = wb$Journey.Purpose$label[c(8,1:7)])
+    factor(ve$Journey_Purpose_of_Driver,
+           labels = c("Data missing or out of range", "Journey as part of work",
+                      "Commuting to/from work", "Taking pupil to/from school", "Pupil riding to/from school",
+                      "Other", "Not known", "Other/Not known (2005-10)"))
   # summary(ve$Journey_Purpose_of_Driver)
-  ve$Sex_of_Driver <- factor(ve$Sex_of_Driver , labels = wb$Sex.of.Driver$label[c(4,1:3)])
+  ve$Sex_of_Driver <- factor(ve$Sex_of_Driver,
+                             labels =
+                               c("Data missing or out of range", "Male", "Female", "Not known"
+                             ))
   levels(ve$Sex_Driver_f)[1] <- levels(ve$Sex_Driver_f)[4]
   summary(ve$Sex_of_Driver)
 
-  wb$IMD.Decile <- dplyr::rename(wb$IMD.Decile, Driver_IMD_Decile = code, IMD_Decile = label)
-
-  ve$Driver_IMD_Decile <- factor(dplyr::inner_join(ve, wb$IMD.Decile)$IMD_Decile)
+  ve$Driver_IMD_Decile <- factor(dplyr::inner_join(ve, structure(list(Driver_IMD_Decile = c(1, 2, 3, 4, 5, 6, 7, 8,
+                                                                                            9, 10, -1), IMD_Decile = c("Most deprived 10%", "More deprived 10-20%",
+                                                                                                                       "More deprived 20-30%", "More deprived 30-40%", "More deprived 40-50%",
+                                                                                                                       "Less deprived 40-50%", "Less deprived 30-40%", "Less deprived 20-30%",
+                                                                                                                       "Less deprived 10-20%", "Least deprived 10%", "Data missing or out of range"
+                                                                                            )), row.names = c(NA, -11L), class = "data.frame", .Names = c("Driver_IMD_Decile",
+                                                                                                                                                          "IMD_Decile")))$IMD_Decile)
 
   ve
 
@@ -255,31 +270,41 @@ read_stats19_ca <- function(data_dir = tempdir(), filename = "Casualties0514.csv
 #' This is a helper function to format raw stats19 data
 #'
 #' @param ca Dataframe representing the raw Stats19 data read-in with \code{read_csv()}.
-#' @param wb List of dataframes containing data labels for stats19 data
 #' @export
 #' @examples
 #' \dontrun{
 #' ca <- format_stats19_ca(ca)
 #' }
-format_stats19_ca <- function(ca, wb){
-
-  data("wb")
+format_stats19_ca <- function(ca){
 
   # nrow(ca) / nrow(ac) # 1.3 casualties per incident: reasonable
-  ca$Casualty_Class <- factor(ca$Casualty_Class, labels = wb$Casualty.Class$label)
+  ca$Casualty_Class <- factor(ca$Casualty_Class,
+                              labels = c("Driver or rider", "Passenger", "Pedestrian"))
 
-  ca$Sex_of_Casualty <- factor(ca$Sex_of_Casualty, labels = wb$Sex.of.Casualty$label[c(3,1,2)])
+  ca$Sex_of_Casualty <- factor(ca$Sex_of_Casualty,
+                               labels = c("Data missing or out of range", "Male", "Female"))
   levels(ca$Sex_of_Casualty)[1] <- "Not known"
 
-  ca$Age_Band_of_Casualty <- factor(ca$Age_Band_of_Casualty, labels = c("na", wb$Age.Band$label[c(1:11)]))
+  ca$Age_Band_of_Casualty <- factor(ca$Age_Band_of_Casualty,
+                                    labels = c("na", "0 - 5", "6 - 10", "11 - 15", "16 - 20", "21 - 25", "26 - 35",
+                                               "36 - 45", "46 - 55", "56 - 65", "66 - 75", "Over 75"))
 
   summary(as.factor(ca$Casualty_Severity))
-  ca$Casualty_Severity <- factor(ca$Casualty_Severity, labels = wb$Casualty.Severity$label)
+  ca$Casualty_Severity <- factor(ca$Casualty_Severity,
+                                 labels = c("Fatal", "Serious", "Slight"))
 
   ca$Casualty_Type <- as.factor(ca$Casualty_Type)
-  ca_type_labs <-
-    wb$Casualty.Type$label[match(levels(ca$Casualty_Type), (wb$Casualty.Type$code))]
-  ca$Type <- factor(ca$Casualty_Type, labels = ca_type_labs)
+  ca$Type <- factor(ca$Casualty_Type,
+                    labels = c("Pedestrian", "Cyclist", "Motorcycle 50cc and under rider or passenger",
+                               "Motorcycle 125cc and under rider or passenger", "Motorcycle over 125cc and up to 500cc rider or  passenger",
+                               "Motorcycle over 500cc rider or passenger", "Taxi/Private hire car occupant",
+                               "Car occupant", "Minibus (8 - 16 passenger seats) occupant",
+                               "Bus or coach occupant (17 or more pass seats)", "Horse rider",
+                               "Agricultural vehicle occupant", "Tram occupant", "Van / Goods vehicle (3.5 tonnes mgw or under) occupant",
+                               "Goods vehicle (over 3.5t. and under 7.5t.) occupant", "Goods vehicle (7.5 tonnes mgw and over) occupant",
+                               "Mobility scooter rider", "Electric motorcycle rider or passenger",
+                               "Other vehicle occupant", "Motorcycle - unknown cc rider or passenger",
+                               "Goods vehicle (unknown weight) occupant"))
 
   ca
 
