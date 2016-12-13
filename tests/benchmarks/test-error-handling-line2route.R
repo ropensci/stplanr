64c7085d332284d7dfe09acfc28d94fe65c777d7
robinlovelace <rob00x@gmail.com>
2016-12-13 11:33:28 +0000

    Add error handling bench

---
+++ tests/benchmarks/test-error-handling-line2route.R
@@ -0,0 +1,9 @@
+devtools::install_github("nikolai-b/stplanr", ref = "add_error_handelling")
+r = line2route(flowlines, reporterror = TRUE)
+r$error # shows error
+
+# now switch off internet partway (manually)
+r = line2route(flowlines, reporterror = TRUE)
+
+r2 = stplanr:::line2routeRetry(flowlines, silent = F)
+
