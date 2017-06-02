e75da60591aefa9c8a1943dc6fb7163d70fcadbd
Robin Lovelace <rob00x@gmail.com>
2017-06-02 22:43:35 +0100

    Add return_all arg

--- R/geo_code.R
+++ R/geo_code.R
@@ -4,19 +4,30 @@
 #'
 #' @param address Text string representing the address you want to geocode
 #' @param base_url The base url to query
+#' @param return_all Should the request return all information returned by Google Maps?
+#'  The default is `FALSE`: to return only two numbers: the longitude and latitude, in that order
 #' @export
 #' @examples
 #' address = "LS7 3HB"
 #' geo_code(address = address)
+#' geo_code(address = address, return_all = TRUE)
 geo_code = function(address,
-                    base_url = "http://maps.google.com/maps/api/geocode/json") {
+                    base_url = "http://maps.google.com/maps/api/geocode/json",
+                    return_all = FALSE) {
 
   q <- list(address = address, sensor = "false")
   u <- httr::modify_url(base_url, query = q)
   res <- jsonlite::fromJSON(u)
   res_df <- jsonlite::flatten(res$results)
-  lat_lon <- c(lat = res_df$geometry.location.lat, lon = res_df$geometry.location.lng)
+  lat_lon <- c(
+    lat = res_df$geometry.location.lat,
+    lon = res_df$geometry.location.lng
+    )
 
-  lat_lon
+  if(return_all) {
+    return(res_df)
+  } else {
+    return(lat_lon)
+  }
 
 }
