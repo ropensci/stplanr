91406564f09ef12f42dd1d991d27daefe8e36cb7
Robin Lovelace <rob00x@gmail.com>
2017-06-02 21:03:38 +0100

    Add geo_code function

---
+++ R/geo_code.R
@@ -0,0 +1,20 @@
+#' Convert text strings into points on the map
+#'
+#' Generate a lat/long pair from data using Google's geolocation API.
+#'
+#' @param address Text string representing the address you want to geocode
+#' @param base_url The base url to query
+#' @export
+#' @examples
+#' address = "LS7 3HB"
+#' geo_code(address = address)
+geo_code = function(address, base_url = "http://maps.google.com", sensor = "false") {
+
+  u <- httr::modify_url(base_url, path = "maps/api/geocode/json", query = q)
+  res = jsonlite::fromJSON(u)
+  res_df = jsonlite::flatten(res$results)
+  lat_lon = c(lat = res_df$geometry.location.lat, lon = res_df$geometry.location.lng)
+
+  lat_lon
+
+}
