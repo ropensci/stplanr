a32f8cbd0cf3a207f7b4ddf8994bbd0a3d5b12c4
Robin Lovelace <rob00x@gmail.com>
2017-07-26 18:21:56 +0100

    Delete old 'gprojected' fun, use sf::

--- R/geo_projected.R
+++ R/geo_projected.R
@@ -4,50 +4,71 @@
 #' shp = sf::st_sf(sf::st_sfc(sf::st_point(c(1, 0))))
 #' geo_select_aeq(shp)
 geo_select_aeq <- function(shp){
-  cent <- st_geometry(shp)
-  coords = st_coordinates(shp)
+  cent <- sf::st_geometry(shp)
+  coords = sf::st_coordinates(shp)
   coords_mat = matrix(coords[,1:2], ncol = 2)
   midpoint = apply(coords_mat, 2, mean)
   aeqd <- sprintf("+proj=aeqd +lat_0=%s +lon_0=%s +x_0=0 +y_0=0",
                   midpoint[1], midpoint[1])
-  st_crs(aeqd)
+  sf::st_crs(aeqd)
 }
 
 #' Perform GIS functions on a temporary, projected version of a spatial object
 #'
 #' This function performs operations on projected data.
+#'
 #' @param shp A spatial object with a geographic (WGS84) coordinate system
+#' @param fun A function to perform on the projected object (e.g.  \code{\link{gLength}}))
 #' @param crs An optional coordinate reference system (if not provided it is set
 #' automatically by \code{\link{crs_select_aeq}}).
+#' @param ... Arguments to pass to \code{fun}, e.g. \code{byid = TRUE} if the function is \code{\link{rgeos::gLength}}))
 #' @export
 #' @examples
-
 #' library(sf)
 #' shp = st_sf(st_sfc(st_point(c(1, 0))))
-
 #' geo_projected(shp, st_buffer, dist = 100)
 #' library(sp)
 #' shp = as(shp, "Spatial")
 #' geo_projected(shp, fun = rgeos::gBuffer, width = 100)
-#' geo_projected(routes_fast, fun = rgeos::gLength, byid = TRUE)
+#' rlength = geo_projected(routes_fast, fun = rgeos::gLength, byid = TRUE)
+#' plot(routes_fast$length, rlength)
 geo_projected = function(x, ...) {
   UseMethod(generic = "geo_projected")
 }
 #' @export
-geo_projected.sf = function(shp, fun, crs_temp = geo_select_aeq(shp),  ...){
+geo_projected.sf = function(shp, fun, crs = geo_select_aeq(shp),  ...){
   # assume it's not projected  (i.e. lat/lon) if there is no CRS
-  if(is.na(st_crs(shp))) st_crs(shp) = 4326
-  crs_orig = st_crs(shp)
-  shp_projected = st_transform(shp, crs_temp)
-  message(paste0("Running function on a temporary projected version of the Spatial object using the CRS: ", crs_temp$proj4string))
+  if(is.na(sf::st_crs(shp))) sf::st_crs(shp) = 4326
+  crs_orig = sf::st_crs(shp)
+  shp_projected = sf::st_transform(shp, crs)
+  message(paste0("Running function on a temporary projected version of the Spatial object using the CRS: ", crs$proj4string))
   res = fun(shp_projected, ...)
   if(grepl("sf", x = class(res)[1]))
-    res = st_transform(res, crs_orig)
+    res = sf::st_transform(res, crs_orig)
   res
 }
 #' @export
-geo_projected.Spatial = function(shp, fun, crs = crs_select_aeq(shp), ...) {
-  gprojected(shp, fun, crs = crs_select_aeq(shp), ...)
+gprojected = # old function name
+  geo_projected.Spatial = function(shp, fun, crs = crs_select_aeq(shp), ...){
+  # assume it's not projected  (i.e. lat/lon) if there is no CRS
+  if(!is.na(is.projected(shp))){
+    if(is.projected(shp)){
+      res = fun(shp, ...)
+    } else {
+      shp_projected = reproject(shp, crs = crs)
+      message(paste0("Running function on a temporary projected version of the Spatial object using the CRS: ", crs))
+      res = fun(shp_projected, ...)
+      if(is(res, "Spatial"))
+        res = spTransform(res, CRS("+init=epsg:4326"))
+    }
+  } else {
+    shp_projected = reproject(shp, crs = crs)
+    message(paste0("Running function on a temporary projected version of the Spatial object using the CRS: ", crs))
+    res = fun(shp_projected, ...)
+    if(is(res, "Spatial"))
+      res = spTransform(res, CRS("+init=epsg:4326"))
+  }
+  res
 }
 #' Perform a buffer operation on a temporary projected CRS
 #'
@@ -64,16 +85,16 @@ geo_projected.Spatial = function(shp, fun, crs = crs_select_aeq(shp), ...) {
 #' routes_fast_sf = sf::st_as_sf(routes_fast)
 #' buff_sf = geo_buffer(routes_fast_sf, dist = 50)
 #' @export
-geo_buffer = function(x, ...) {
+geo_buffer = function(shp, ...) {
   UseMethod("geo_buffer")
 }
 
 #' @export
-geo_buffer.sf = function(shp, dist = 0, ...) {
-  geo_projected(shp, sf::st_buffer, dist = dist)
+geo_buffer.sf = function(shp, ...) {
+  geo_projected(shp, sf::st_buffer, ...)
 }
 
 #' @export
-geo_buffer.Spatial = function(shp, dist = 0, ...) {
-  buff_geo(shp = shp, width = dist, ...)
+geo_buffer.Spatial = function(shp, ...) {
+  geo_projected.Spatial(shp, rgeos::gBuffer, width = dist)
 }
