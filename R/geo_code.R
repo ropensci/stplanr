c3341f7ef14d2630b85b2a4b7a9996af52738fb0
Robin Lovelace <rob00x@gmail.com>
2017-10-02 14:53:14 +0100

    Update geo_code to use https and add optional key

--- R/geo_code.R
+++ R/geo_code.R
@@ -6,18 +6,30 @@
 #' @param base_url The base url to query
 #' @param return_all Should the request return all information returned by Google Maps?
 #'  The default is `FALSE`: to return only two numbers: the longitude and latitude, in that order
+#' @inheritParams route_cyclestreet
 #' @export
 #' @examples
+#' \dontrun{
 #' address <- "LS7 3HB"
 #' geo_code(address = address)
 #' geo_code(address = address, return_all = TRUE)
+#' geo_code(address = address, pat = Sys.getenv("GOOGLE")) # needs api key in .Renviron
+#' }
 geo_code <- function(address,
-                    base_url = "http://maps.google.com/maps/api/geocode/json",
-                    return_all = FALSE) {
+                    base_url = "https://maps.google.com/maps/api/geocode/json",
+                    return_all = FALSE,
+                    pat = NULL
+                    ) {
 
   q <- list(address = address, sensor = "false")
+  if(!is.null(pat)) {
+    q <- c(q, key = pat)
+  }
   u <- httr::modify_url(base_url, query = q)
   res <- jsonlite::fromJSON(u)
+  if(res$status == "OVER_QUERY_LIMIT") {
+    stop(res$error_message)
+  }
   res_df <- jsonlite::flatten(res$results)
   lon_lat <- c(
     lon = res_df$geometry.location.lng,
