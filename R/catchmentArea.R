5afbe1cd95a834b6ec084a5888315160650ebc40
Robin Lovelace <rob00x@gmail.com>
2019-01-03 00:01:03 +0000

    Changes for new dplyr

--- R/catchmentArea.R
+++ R/catchmentArea.R
@@ -50,8 +50,8 @@
 #' data_dir <- system.file("extdata", package = "stplanr")
 #' unzip(file.path(data_dir, "smallsa1.zip"))
 #' unzip(file.path(data_dir, "testcycleway.zip"))
-#' sa1income <- readOGR(".", "smallsa1")
-#' testcycleway <- readOGR(".", "testcycleway")
+#' sa1income <- rgdal::readOGR(".", "smallsa1")
+#' testcycleway <- rgdal::readOGR(".", "testcycleway")
 #' calc_catchment(
 #'   polygonlayer = sa1income,
 #'   targetlayer = testcycleway,
@@ -60,6 +60,21 @@
 #'   projection = "austalbers",
 #'   dissolve = TRUE
 #' )
+#' sa1income <- sf::read_sf("smallsa1.shp")
+#' testcycleway <- sf::read_sf("testcycleway.shp")
+#' f = list.files(".", "testcycleway|smallsa1")
+#' file.remove(f)
+#' cway_catch <- calc_catchment(
+#'   polygonlayer = sa1income,
+#'   targetlayer = testcycleway,
+#'   calccols = c("Total"),
+#'   distance = 800,
+#'   projection = "austalbers",
+#'   dissolve = TRUE
+#' )
+#' plot(sa1income$geometry)
+#' plot(testcycleway$geometry, col = "red", add = TRUE)
+#' plot(cway_catch["Total"], add = TRUE)
 #' }
 calc_catchment <- function(
                            polygonlayer,
@@ -308,7 +323,7 @@ calc_catchment.sf <- function(
 #'   distance = 800,
 #'   projection = "austalbers"
 #' )
-#' 
+#'
 #' calc_catchment_sum(
 #'   polygonlayer = sa1income,
 #'   targetlayer = testcycleway,
