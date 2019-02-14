1b7be12eab46bfd183198ed46941fb96bbe3637e
Robin Lovelace <rob00x@gmail.com>
2019-02-14 02:00:23 +0000

    Document

---
+++ R/depreciated.R
@@ -0,0 +1,8 @@
+#' Deprecated functions in stplanr
+#'
+#' These functions are depreciated and will be removed:
+#'
+#' - [dl_stats19()]: see the [stats19](https://github.com/ropensci/stats19) package
+#'
+#' @name stplanr-deprecated
+NULL
