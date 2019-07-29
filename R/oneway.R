4d70d6763a48961d040e58eee9a59fa09b3dfb56
Robin Lovelace <rob00x@gmail.com>
2019-07-29 23:05:46 +0100

    Reduce build times in benchmark

--- R/oneway.R
+++ R/oneway.R
@@ -176,14 +176,12 @@ od_id_order <- function(x, id1 = names(x)[1], id2 = names(x)[2]) {
 #' @seealso od_oneway
 #' @name od_id
 #' @examples
-#' d <- od_data_sample[2:9, 1:2]
-#' d
-#' id <- od_id_character(d[[1]], d[[2]])
-#' id
+#' (d <- od_data_sample[2:9, 1:2])
+#' (id <- od_id_character(d[[1]], d[[2]]))
 #' duplicated(id)
 #' od_id_szudzik(d[[1]], d[[2]])
 #' od_id_max_min(d[[1]], d[[2]])
-#' n <- 1000
+#' n <- 100
 #' ids <- as.character(runif(n, 1e4, 1e7 - 1))
 #' # benchmark of methods:
 #' x <- data.frame(
@@ -192,8 +190,7 @@ od_id_order <- function(x, id1 = names(x)[1], id2 = names(x)[2]) {
 #'   val = 1,
 #'   stringsAsFactors = FALSE
 #' )
-#' bench::mark(
-#'   check = FALSE,
+#' bench::mark(check = FALSE, iterations = 10,
 #'   od_id_order(x),
 #'   od_id_character(x$id1, x$id2),
 #'   od_id_szudzik(x$id1, x$id2),
