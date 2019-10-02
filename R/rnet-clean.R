982c7f08e3c9306422a2a4c35d210af8a392f59b
Robin Lovelace <rob00x@gmail.com>
2019-10-02 18:13:50 +0100

    Update rnet_breakup_vertices(), fix #282

--- R/rnet-clean.R
+++ R/rnet-clean.R
@@ -87,7 +87,7 @@ rnet_breakup_vertices <- function(rnet, breakup_internal_vertex_matches = TRUE) 
     rnet_breakup_collection <- lwgeom::st_split(rnet, rnet_internal_vertexes_duplicated)
     rnet_clean <- sf::st_collection_extract(rnet_breakup_collection, "LINESTRING")
   } else {
-    rnet_clean <- rnet
+    rnet_clean <- rnet_clean
   }
 
   rnet_clean
