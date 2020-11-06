b12e17b197150b6fae073f22f92f97d3aef616a4
Robin Lovelace <rob00x@gmail.com>
2020-11-06 23:34:46 +0000

    Another typo fix

--- R/route.R
+++ R/route.R
@@ -29,7 +29,7 @@
 #'   sln = sln
 #' )
 #' library(sf) # for plotting
-#' plot(sp["all"], add = TRUE, lwd = 5)
+#' plot(r_local["all"], add = TRUE, lwd = 5)
 route <- function(from = NULL, to = NULL, l = NULL,
                   route_fun = cyclestreets::journey,
                   n_print = 10, list_output = FALSE, cl = NULL, ...) {
