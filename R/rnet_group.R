cf39a980f13a4bf185d3865002bf14f246894180
Robin Lovelace <rob00x@gmail.com>
2020-11-08 13:11:26 +0000

    Add as.undirected argument

--- R/rnet_group.R
+++ R/rnet_group.R
@@ -5,6 +5,9 @@
 #' @param d Optional distance variable used to classify segments that are
 #' close (within a certain distance specified by `d`) to each other but not
 #' necessarily touching
+#' @param as.undirected Coerce the graph created internally into an undirected
+#' graph with [igraph::as.undirected()]? TRUE by default, which enables use
+#' of a wider range of clutering functions.
 #' @inheritParams rnet_breakup_vertices
 #'
 #' @return The function returns an integer vector reporting the group of each network element
@@ -24,16 +27,16 @@
 #' rnet$group_fast_greedy = rnet_group(rnet, igraph::cluster_fast_greedy)
 #' plot(rnet["group_fast_greedy"])
 #' @export
-rnet_group <- function(rnet, cluster_fun = igraph::clusters, d = NULL) {
+rnet_group <- function(rnet, cluster_fun = igraph::clusters, d = NULL, as.undirected = TRUE) {
   if(!is.null(d)) {
     touching_list = sf::st_is_within_distance(rnet, dist = d)
   } else {
     touching_list = sf::st_intersects(rnet)
   }
   g = igraph::graph.adjlist(touching_list)
-  # igraph::is.directed(g)
-  #> [1] TRUE
-  g = igraph::as.undirected(g)
+  if(as.undirected) {
+    g = igraph::as.undirected(g)
+  }
   wc = cluster_fun(g)
   m = igraph::membership(wc)
   class(m)
