de5ef4cfca8320832b58b994baf0f8d89c3f2bed
Robin Lovelace <rob00x@gmail.com>
2020-11-22 21:39:05 +0000

    Add rnet_duplicated_vertices() function

--- R/rnet_boundary_points.R
+++ R/rnet_boundary_points.R
@@ -8,6 +8,7 @@
 #' rnet <- rnet_roundabout
 #' bp1 <- rnet_boundary_points(rnet)
 #' bp2 <- rnet_boundary_points_lwgeom(rnet) # slower version with lwgeom
+#' bp3 <- line2points(rnet) # slower version with lwgeom
 #' identical(sort(sf::st_coordinates(bp1)), sort(sf::st_coordinates(bp2)))
 #' plot(rnet$geometry)
 #' plot(bp1, add = TRUE)
@@ -36,4 +37,47 @@ rnet_boundary_points_lwgeom <- function(rnet) {
   duplicates <- duplicated(start_and_end_points)
   sf::st_sf(start_and_end_points[!duplicates])
 }
-# bench::mark(check = FALSE, rnet_boundary_points(rnet), rnet_boundary_points_lwgeom(rnet))
+#' @rdname rnet_boundary_points
+#' @param n The minimum number of time a vertex must be duplicated to be returned
+#' @export
+rnet_duplicated_vertices <- function(rnet, n = 2) {
+  coords <- sfheaders::sf_to_df(rnet)
+  coords_n_all <- stats::aggregate(coords$sfg_id, list(coords$x, coords$y), FUN = length)
+  names(coords_n_all) <- c("x", "y", "n")
+  # coords_n_all = dplyr::count(coords, x, y) # v. slow
+  coords_n <- coords_n_all[coords_n_all$n >= n, -ncol(coords_n_all)]
+  p <- sfheaders::sf_point(coords_n)
+  sf::st_crs(p) <- sf::st_crs(rnet)
+  # data.table implementation (to check)
+  # if(requireNamespace("data.table", quietly = TRUE)) {
+  #   coords_dt = data.table::setDT(coordinates[, c("x", "y")])
+  #   coords_n_all = coords_dt[, data.table::.N]
+  #   coords_n = coords_n_all[coords_n_all$N >= n]
+  #   p <- sfheaders::sf_point(coords_n[, -ncol(coords_n)])
+  #   sf::st_crs(p) <- sf::st_crs(rnet)
+  # }
+  p
+}
+
+# bench::mark(check = FALSE,
+#             rnet_boundary_points(rnet),
+#             rnet_boundary_points_lwgeom(rnet),
+#             rnet_duplicated_vertices(rnet),
+#             line2points(rnet)
+# )
+# A tibble: 4 x 13
+# expression                             min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc
+# <bch:expr>                        <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>
+#   1 rnet_boundary_points(rnet)           372µs 384.58µs     2559.     331KB     15.0  1198     7
+# 2 rnet_boundary_points_lwgeom(rnet)   1.21ms   1.25ms      783.     249KB     15.1   363     7
+# 3 rnet_duplicated_vertices(rnet)      1.13ms   1.15ms      864.     273KB     10.6   408     5
+# 4 line2points(rnet)                   1.93ms   1.97ms      501.     265KB     13.0   231     6
+# # … with 5 more variables: total_time <bch:tm>, result <list>, memory <list>, time <list>,
+# #   gc <list>
+
+# coords_single <- stplanr::od_id_szudzik(coords$x, coords$y)
+# coords_count <- table(coords_single)
+# coords_in <- coords[coords_single %in% names(coords_count)[coords_count >= 3], ]
+# coords_n <- unique(coords_in[-c(1, 2)])
+
+
