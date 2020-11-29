8f055db02e07e83be18e81052ca971e5f9f394aa
Robin Lovelace <rob00x@gmail.com>
2020-11-29 00:17:43 +0000

    Add test code for #416

--- data-raw/ad-hoc-tests/test-rnet-breakup-timing1.R
+++ data-raw/ad-hoc-tests/test-rnet-breakup-timing1.R
@@ -1,9 +1,11 @@
 # Aim: try to speed-up rnet_breakup_vertices
 
+remotes::install_github("ropensci/stplanr")
+
 library(sf)
 library(stplanr)
-rnet = stplanr::rnet_roundabout
-rnet = stplanr::osm_net_example
+# rnet = stplanr::rnet_roundabout
+# rnet = stplanr::osm_net_example
 rnet = trafficalmr::tc_data_osm
 
 rnet_boundary_rnet_breakup <- function(rnet) {
@@ -20,28 +22,51 @@ rnet_boundary_rnet_breakup <- function(rnet) {
     )
 }
 
-rnet_boundary_rnet_breakup2 <- function(rnet) {
-  rnet_nodes <- sfheaders::sf_to_df(rnet)
-  rnet_internal_vertexes <- sf::st_geometry(line2vertices(rnet))
-  unique_rnet_nodes <- do.call("c", unique(rnet_nodes))
-  unique_rnet_internal_vertexes <- do.call("c", unique(rnet_internal_vertexes))
-  rbind_nodes_internal_vertexes <- rbind(unique_rnet_nodes, unique_rnet_internal_vertexes)
-  index_intersection_points <- duplicated(rbind_nodes_internal_vertexes)
-  intersection_points <- sf::st_as_sf(
-    data.frame(rbind_nodes_internal_vertexes[index_intersection_points, , drop = FALSE]),
-    coords = c("x_coords", "y_coords"),
-    crs = sf::st_crs(rnet)
-  )
+`%dtIN%` <- function(y, x) {
+  tmp = data.table::rbindlist(list(x,y))
+  len_ = nrow(x)
+  tmp[, idx := any(.I <= len_) & .N > 1L, by=names(tmp)]
+  tail(tmp$idx, nrow(y))
 }
+
+rnet_boundary_rnet_breakup2 <- function(rnet) {
+  coordinates <- sfheaders::sf_to_df(rnet)
+  first_pair <- !duplicated(coordinates[["sfg_id"]])
+  last_pair <- !duplicated(coordinates[["sfg_id"]], fromLast = TRUE)
+  idxs <- first_pair | last_pair
+  pairs <- unique(coordinates[idxs, c("x", "y")])
+  i <- coordinates[!idxs, c("x", "y")]
+  # find 'internal intersections'
+  i_dup <- duplicated(i)
+  i_u <- unique(i)
+  # this stage can be made a bit faster with data.table:
+  # https://stackoverflow.com/questions/23971160
+  # i_in_pairs = interaction(i_u) %in% interaction(pairs) # very slow!
+  if(requireNamespace("data.table", quietly = TRUE))
+  i_in_pairs <- i_u %dtIN% pairs
+  p_sf <- rbind(i_u[i_in_pairs, ], i[i_dup, ])
+  p <- sfheaders::sf_point(unique(p_sf))
+  sf::st_crs(p) <- sf::st_crs(rnet)
+  p
+}
+
+p1 = rnet_boundary_rnet_breakup(rnet)
+p2 = rnet_boundary_rnet_breakup2(rnet)
+
+nrow(p1)
+nrow(p2)
+plot(p1)
+plot(p2, cex = 2, add = TRUE)
 
 bpline2points <- line2points(rnet)
 boundary_points <- rnet_boundary_points(rnet)
 boundary_points2 <- rnet_duplicated_vertices(rnet)
 boundary_points_rnb <- rnet_boundary_rnet_breakup(rnet)
+boundary_points_rnb2 <- rnet_boundary_rnet_breakup2(rnet)
 plot(rnet$geometry)
-plot(boundary_points, add = TRUE, cex = 0.2) # too many
+plot(boundary_points, add = TRUE, cex = 0.5) # too many
 plot(boundary_points2, add = TRUE)
-plot(boundary_points_rnb, col = "red", add = TRUE)
+plot(boundary_points_rnb, col = "red", add = TRUE, cex = 3)
 
 nrow(bpline2points)
 nrow(boundary_points)
@@ -56,9 +81,33 @@ bench::mark(check = FALSE,
   rbp = {rnet_boundary_points(rnet)},
   rdv = {rnet_duplicated_vertices(rnet)},
   rnb = {rnet_boundary_rnet_breakup(rnet)},
+  rnb2 = {rnet_boundary_rnet_breakup2(rnet)},
   rnbv = {rnet_breakup_vertices(rnet)}
 )
 
+library(profvis)
+profvis({
+  coordinates <- sfheaders::sf_to_df(trafficalmr::tc_data_osm)
+  first_pair <- !duplicated(coordinates[["sfg_id"]])
+  last_pair <- !duplicated(coordinates[["sfg_id"]], fromLast = TRUE)
+  idxs <- first_pair | last_pair
+  pairs <- unique(coordinates[idxs, c("x", "y")])
+  i <- coordinates[!idxs, c("x", "y")]
+  # find 'internal intersections'
+  i_dup <- duplicated(i)
+  i_u <- unique(i)
+  # this stage can be made a bit faster with data.table:
+  # https://stackoverflow.com/questions/23971160
+  # i_in_pairs = interaction(i_u) %in% interaction(pairs) # very slow!
+  tmp <- data.table::rbindlist(list(i_u, pairs))
+  len_ <- nrow(i_u)
+  tmp[, idx := any(.I <= len_) & .N > 1L, by=names(tmp)]
+  i_in_pairs <- tail(tmp$idx, nrow(pairs))
+  p_sf <- rbind(i_u[i_in_pairs, ], i[i_dup, ])
+  p <- sfheaders::sf_point(unique(p_sf))
+  sf::st_crs(p) <- sf::st_crs(rnet)
+  p
+})
 
 
 rnet_breakup_vertices2 <- function(rnet, breakup_internal_vertex_matches = TRUE) {
