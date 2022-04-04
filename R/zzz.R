c8da0c49c5359a3421040eebcf8c92e8f29ffc76
Robin Lovelace <rob00x@gmail.com>
2022-04-04 21:15:53 +0100

    Update msg again

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onLoad <- function(libname, pkgname){
   packageStartupMessage(
     paste0(
-      "Loading stplanr v1.0.0.\n",
-      "Note: this version removes support for sp objects.\n",
+      "Loading stplanr v0.9.0.\n",
+      "Note: the next planned version, v1.0.0, will not support sp objects.\n",
       "See the issue #332 and https://github.com/ropensci/stplanr/pull/481.",
       "Any feedback on GitHub: welcome. Thanks (Robin Lovelace, April 2022)!"
     )
