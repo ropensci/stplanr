733091a19749e696dc0ce895dcb594ce90405f26
robinlovelace <rob00x@gmail.com>
2022-11-13 22:35:39 +0000

    Update docs

--- R/rnet_join.R
+++ R/rnet_join.R
@@ -13,6 +13,9 @@
 #' There are rarely 1-to-1 relationships between spatial network geometries
 #' so we take care when using this function.
 #'
+#' See [#505](https://github.com/ropensci/stplanr/issues/505) for details
+#' and a link to an interactive example of inputs and outputs shown below.
+#'
 #' @param rnet_x Target route network, the output will have the same geometries
 #'   as features in this object.
 #' @param rnet_y Source route network. Columns from this route network object will
@@ -21,19 +24,25 @@
 #' @param length_y Add a new column called `length_y`? Useful when joining based on
 #'   length of segments (e.g. weighted mean). `TRUE` by default.
 #' @param key_column The index of the key (unique identifier) column in `rnet_x`.
+#' @param subset_x Subset the source route network by the target network before
+#'   creating buffers? This can lead to faster and better results. Default:
+#'   `TRUE`.
+#' @param dist_subset The buffer distance in m to apply when breaking up the
+#'   source object `rnet_y`. Default: 5.
 #' @param split_y Should the second route network be split at the start and
 #'   end points of LINESTRING features in the first? `TRUE` by default.
+#' @param ... Additional arguments passed to `rnet_subset`.
 #' @examples
 #' library(sf)
 #' library(dplyr)
 #' # Uncomment for interactive examples:
-#' # library(mapview)
 #' plot(route_network_small["flow"])
 #' plot(osm_net_example$geometry, lwd = 5, col = "grey")
 #' plot(route_network_small["flow"], add = TRUE)
 #' rnetj = rnet_join(osm_net_example, route_network_small, dist = 9)
-#' mapview(rnetj, zcol = "flow") +
-#'   mapview(route_network_small, zcol = "flow")
+#' # library(mapview)
+#' # mapview(rnetj, zcol = "flow") +
+#' #  mapview(route_network_small, zcol = "flow")
 #' plot(rnetj["flow"])
 #' plot(route_network_small["flow"], add = TRUE)
 #' rnetj_summary = rnetj %>%
@@ -43,16 +52,22 @@
 #'       flow = weighted.mean(flow, length_y, na.rm = TRUE),
 #'       )
 #' osm_joined_rnet = left_join(osm_net_example, rnetj_summary)
-#' plot(osm_joined_rnet[c("flow", "highway")])
-#' mapview(joined_network) +
-#'   mapview(route_network_small)
+#' plot(route_network_small["flow"])
+#' plot(osm_joined_rnet[c("flow")])
+#' # Improve fit between geometries and performance by subsetting rnet_x
+#' osm_subset = rnet_subset(osm_net_example, route_network_small, dist = 5)
+#' osm_joined_rnet = left_join(osm_subset, rnetj_summary)
+#' plot(route_network_small["flow"])
+#' plot(osm_joined_rnet[c("flow")])
+#' # mapview(joined_network) +
+#' #   mapview(route_network_small)
 #' @export
 rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
                      subset_x = TRUE, dist_subset = 5, split_y = TRUE, ...) {
-  rnet_x_buffer = geo_buffer(rnet_x, dist = dist, nQuadSegs = 2)
   if (subset_x) {
     rnet_x = rnet_subset(rnet_x, rnet_y, dist = dist_subset, ...)
   }
+  rnet_x_buffer = geo_buffer(rnet_x, dist = dist, nQuadSegs = 2)
   if (split_y) {
     rnet_y = rnet_split_lines(rnet_y, rnet_x, dist = dist_subset)
   }
@@ -72,6 +87,7 @@ rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
 #' @param min_x Segments shorter than this multiple of dist
 #'   *and* which were longer
 #'   before the cropping process will be removed. 3 by default.
+#' @export
 rnet_subset = function(rnet_x, rnet_y, dist = 1, crop = TRUE, min_x = 3) {
   rnet_x$length_x_original = as.numeric(sf::st_length(rnet_x))
   rnet_y_union = sf::st_union(rnet_y)
@@ -98,6 +114,7 @@ rnet_subset = function(rnet_x, rnet_y, dist = 1, crop = TRUE, min_x = 3) {
 #' @param geo_y The geographic object used to break up the route network
 #' @param dist The width of the buffer used when breaking up the route network.
 #'   For imprecise data it may be worth increasing this above 1 m, the default.
+#' @export
 rnet_split_lines = function(rnet_x, geo_y, dist = 1) {
   if (all(grepl(pattern = "LINE", x = sf::st_geometry_type(rnet_x)))) {
     geo_y = c(
@@ -118,6 +135,7 @@ rnet_split_lines = function(rnet_x, geo_y, dist = 1) {
 #' Convert multilinestring object into linestrings, without losing and vertices
 #'
 #' @param x LInestring object
+#' @export
 line_cast = function(x) {
   sf::st_cast(sf::st_cast(x, "MULTILINESTRING"), "LINESTRING")
 }
