4444c7e06df8b121be454d872b8db8cbf5e1b5e3
Robin Lovelace <Robinlovelace@users.noreply.github.com>
2023-08-16 00:15:34 +0100

    Starter on 515 (#516)
    * Starter on 515
    
    * Update rnet_split_lines
    
    * Use centroids
    
    * Debugging #514
    
    * Fix #514
    
    * Document
    
    * Vectorise line_segment(), close #518
    
    * Tests passing woop
    
    * Implement rnet_merge()
    
    * Tests pass, again!

--- R/rnet_join.R
+++ R/rnet_join.R
@@ -32,8 +32,10 @@
 #'   `TRUE`.
 #' @param dist_subset The buffer distance in m to apply when breaking up the
 #'   source object `rnet_y`. Default: 5.
-#' @param split_y Should the second route network be split at the start and
-#'   end points of LINESTRING features in the first? `TRUE` by default.
+#' @param segment_length Should the source route network be split?
+#'   `0` by default, meaning no splitting. Values above 0 split the source
+#'   into linestrings with a max distance. Around 5 (m) may be a sensible
+#'   default for many use cases, the smaller the value the slower the process.
 #' @param ... Additional arguments passed to `rnet_subset`.
 #' @examples
 #' library(sf)
@@ -43,20 +45,28 @@
 #' plot(osm_net_example$geometry, lwd = 5, col = "grey")
 #' plot(route_network_small["flow"], add = TRUE)
 #' rnetj = rnet_join(osm_net_example, route_network_small, dist = 9)
+#' rnetj2 = rnet_join(osm_net_example, route_network_small, dist = 9, segment_length = 10)
 #' # library(mapview)
 #' # mapview(rnetj, zcol = "flow") +
-#' #  mapview(route_network_small, zcol = "flow")
-#' plot(rnetj["flow"])
+#' #   mapview(rnetj2, zcol = "flow") +
+#' #   mapview(route_network_small, zcol = "flow")
+#' plot(sf::st_geometry(rnetj))
+#' plot(rnetj["flow"], add = TRUE)
+#' plot(rnetj2["flow"], add = TRUE)
 #' plot(route_network_small["flow"], add = TRUE)
-#' rnetj_summary = rnetj %>%
+#' summary(rnetj2$length_y)
+#' rnetj_summary = rnetj2 %>%
+#'   filter(!is.na(length_y)) %>%
 #'   sf::st_drop_geometry() %>%
 #'   group_by(osm_id) %>%
 #'     summarise(
 #'       flow = weighted.mean(flow, length_y, na.rm = TRUE),
 #'       )
 #' osm_joined_rnet = left_join(osm_net_example, rnetj_summary)
-#' plot(route_network_small["flow"])
-#' plot(osm_joined_rnet[c("flow")])
+#' plot(sf::st_geometry(route_network_small))
+#' plot(route_network_small["flow"], lwd = 3, add = TRUE)
+#' plot(sf::st_geometry(osm_joined_rnet), add = TRUE)
+#' plot(osm_joined_rnet[c("flow")], lwd = 9, add = TRUE)
 #' # Improve fit between geometries and performance by subsetting rnet_x
 #' osm_subset = rnet_subset(osm_net_example, route_network_small, dist = 5)
 #' osm_joined_rnet = left_join(osm_subset, rnetj_summary)
@@ -66,18 +76,20 @@
 #' #   mapview(route_network_small)
 #' @export
 rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
-                     subset_x = TRUE, dist_subset = 5, split_y = TRUE, ...) {
+                     subset_x = TRUE, dist_subset = 5, segment_length = 0, ...) {
+  # browser()
   if (subset_x) {
     rnet_x = rnet_subset(rnet_x, rnet_y, dist = dist_subset, ...)
   }
   rnet_x_buffer = geo_buffer(rnet_x, dist = dist, nQuadSegs = 2)
-  if (split_y) {
-    rnet_y = rnet_split_lines(rnet_y, rnet_x, dist = dist_subset)
+  if (segment_length > 0) {
+    rnet_y = line_segment(rnet_y, segment_length = segment_length)
   }
   if (length_y) {
     rnet_y$length_y = as.numeric(sf::st_length(rnet_y))
   }
-  rnetj = sf::st_join(rnet_x_buffer[key_column], rnet_y, join = sf::st_contains)
+  rnet_y_centroids = sf::st_centroid(rnet_y)
+  rnetj = sf::st_join(rnet_x_buffer[key_column], rnet_y_centroids)
   rnetj
 }
 
@@ -91,7 +103,7 @@ rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
 #'   *and* which were longer
 #'   before the cropping process will be removed. 3 by default.
 #' @export
-rnet_subset = function(rnet_x, rnet_y, dist = 1, crop = TRUE, min_x = 3) {
+rnet_subset = function(rnet_x, rnet_y, dist = 10, crop = TRUE, min_x = 3) {
   rnet_x$length_x_original = as.numeric(sf::st_length(rnet_x))
   rnet_y_union = sf::st_union(rnet_y)
   rnet_y_buffer = stplanr::geo_buffer(rnet_y_union, dist = dist, nQuadSegs = 2)
@@ -108,32 +120,6 @@ rnet_subset = function(rnet_x, rnet_y, dist = 1, crop = TRUE, min_x = 3) {
   }
   rnet_x
 }
-#' Split lines in a route network based points
-#'
-#' If the object passed to the second argument has LINSTRING geometries
-#'   the start and end points of linestrings are used.
-#'
-#' @param rnet_x The route network to be broken into smaller pieces
-#' @param geo_y The geographic object used to break up the route network
-#' @param dist The width of the buffer used when breaking up the route network.
-#'   For imprecise data it may be worth increasing this above 1 m, the default.
-#' @export
-rnet_split_lines = function(rnet_x, geo_y, dist = 1) {
-  if (all(grepl(pattern = "LINE", x = sf::st_geometry_type(rnet_x)))) {
-    geo_y = c(
-      lwgeom::st_startpoint(geo_y),
-      lwgeom::st_endpoint(geo_y)
-    )
-  }
-  # speed-up subsequent steps:
-  points = sf::st_union(geo_y)
-  points_buffer = stplanr::geo_buffer(points, dist = dist)
-  rnet_split = sf::st_difference(rnet_x, points_buffer)
-  rnet_split_lines = line_cast(rnet_split)
-  rnet_split_lines$length_osm_cast = as.numeric(sf::st_length(rnet_split_lines))
-  # rnet_split_lines[rnet_split_lines$length_osm_cast > min_lenth, ]
-  rnet_split_lines
-}
 
 #' Convert multilinestring object into linestrings
 #'
@@ -143,4 +129,73 @@ rnet_split_lines = function(rnet_x, geo_y, dist = 1) {
 #' @export
 line_cast = function(x) {
   sf::st_cast(sf::st_cast(x, "MULTILINESTRING"), "LINESTRING")
+}
+
+#' Merge route networks, keeping attributes with aggregating functions
+#'
+#' @inheritParams rnet_join
+#' @param sum_flows Should flows be summed? `TRUE` by default.
+#' @param funs A named list of functions to apply to named columns, e.g.:
+#'   `list(flow = sum, length = mean)`. The default is to sum all numeric
+#'   columns.
+#' @param ... Additional arguments passed to `rnet_join`.
+#' @export
+#' @examples
+#' # The target object
+#' rnet_x = rnet_subset(osm_net_example[1], route_network_small)
+#' # The source object:
+#' rnet_y = route_network_small["flow"]
+#' rnet_y$quietness = rnorm(nrow(rnet_y))
+#' funs = list(flow = sum, quietness = mean)
+#' rnet_merged = rnet_merge(rnet_x[1], rnet_y[c("flow", "quietness")],
+#'                          dist = 9, segment_length = 20, funs = funs)
+#' plot(rnet_y["flow"])
+#' plot(rnet_merged["flow"])
+#' @return An sf object with the same geometry as `rnet_x`
+rnet_merge <- function(rnet_x, rnet_y, dist = 5, funs = NULL, sum_flows = TRUE, ...) {
+  if (is.null(funs)) {
+    funs = list()
+    for (col in names(rnet_y)) {
+      if (is.numeric(rnet_y[[col]])) {
+        funs[[col]] = sum
+      }
+    }
+  }
+  sum_cols = sapply(funs, function(f) identical(f, sum))
+  sum_cols = names(funs)[which(sum_cols)]
+  rnetj = rnet_join(rnet_x, rnet_y, dist = dist, ...)
+  names(rnetj)
+  rnetj_df = sf::st_drop_geometry(rnetj)
+  # Apply functions to columns with lapply:
+  res_list = lapply(seq(length(funs)), function(i) {
+    nm = names(funs[i])
+    fn = funs[[i]]
+
+    if (identical(fn, sum) && sum_flows) {
+      res = rnetj_df %>%
+        dplyr::group_by_at(1) %>%
+        dplyr::summarise(dplyr::across(dplyr::matches(nm), function(x) sum(x * length_y)))
+    } else {
+      res = rnetj_df %>%
+        dplyr::group_by_at(1) %>%
+        dplyr::summarise(dplyr::across(dplyr::matches(nm), fn))
+    }
+    names(res)[2] = nm
+    if(i > 1) {
+      res = res[-1]
+    }
+    res
+  })
+  res_df = dplyr::bind_cols(res_list)
+  res_sf = dplyr::left_join(rnet_x, res_df)
+  if (sum_flows) {
+    res_sf$length_x = as.numeric(sf::st_length(res_sf))
+    for(i in sum_cols) {
+      # i = sum_cols[1]
+      res_sf[[i]] = res_sf[[i]] / res_sf$length_x
+      over_estimate = sum(res_sf[[i]], na.rm = TRUE) / sum(rnet_y[[i]], na.rm = TRUE)
+      res_sf[[i]] = res_sf[[i]] / over_estimate
+    }
+  }
+  res_sf
 }
