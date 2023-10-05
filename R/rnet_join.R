99d114af25e5455e9eb8d2a96e9f6c21cbffb543
robinlovelace <rob00x@gmail.com>
2023-10-05 12:22:31 +0100

    Format rnet_join()

--- R/rnet_join.R
+++ R/rnet_join.R
@@ -50,8 +50,8 @@
 #' library(dplyr)
 #' plot(osm_net_example$geometry, lwd = 5, col = "grey", add = TRUE)
 #' plot(route_network_small["flow"], add = TRUE)
-#' rnetj = rnet_join(osm_net_example, route_network_small, dist = 9)
-#' rnetj2 = rnet_join(osm_net_example, route_network_small, dist = 9, segment_length = 10)
+#' rnetj <- rnet_join(osm_net_example, route_network_small, dist = 9)
+#' rnetj2 <- rnet_join(osm_net_example, route_network_small, dist = 9, segment_length = 10)
 #' # library(mapview)
 #' # mapview(rnetj, zcol = "flow") +
 #' #   mapview(rnetj2, zcol = "flow") +
@@ -61,65 +61,64 @@
 #' plot(rnetj2["flow"], add = TRUE)
 #' plot(route_network_small["flow"], add = TRUE)
 #' summary(rnetj2$length_y)
-#' rnetj_summary = rnetj2 %>%
+#' rnetj_summary <- rnetj2 %>%
 #'   filter(!is.na(length_y)) %>%
 #'   sf::st_drop_geometry() %>%
 #'   group_by(osm_id) %>%
-#'     summarise(
-#'       flow = weighted.mean(flow, length_y, na.rm = TRUE),
-#'       )
-#' osm_joined_rnet = dplyr::left_join(osm_net_example, rnetj_summary)
+#'   summarise(
+#'     flow = weighted.mean(flow, length_y, na.rm = TRUE),
+#'   )
+#' osm_joined_rnet <- dplyr::left_join(osm_net_example, rnetj_summary)
 #' plot(sf::st_geometry(route_network_small))
 #' plot(route_network_small["flow"], lwd = 3, add = TRUE)
 #' plot(sf::st_geometry(osm_joined_rnet), add = TRUE)
 #' # plot(osm_joined_rnet[c("flow")], lwd = 9, add = TRUE)
 #' # Improve fit between geometries and performance by subsetting rnet_x
-#' osm_subset = rnet_subset(osm_net_example, route_network_small, dist = 5)
-#' osm_joined_rnet = dplyr::left_join(osm_subset, rnetj_summary)
+#' osm_subset <- rnet_subset(osm_net_example, route_network_small, dist = 5)
+#' osm_joined_rnet <- dplyr::left_join(osm_subset, rnetj_summary)
 #' plot(route_network_small["flow"])
 #' # plot(osm_joined_rnet[c("flow")])
 #' # mapview(joined_network) +
 #' #   mapview(route_network_small)
 #' @export
-rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
-                     subset_x = TRUE, dist_subset = NULL, segment_length = 0,
-                     endCapStyle = "FLAT", contains = TRUE, max_angle_diff = NULL,
-                     crs = geo_select_aeq(rnet_x), ...) {
+rnet_join <- function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
+                      subset_x = TRUE, dist_subset = NULL, segment_length = 0,
+                      endCapStyle = "FLAT", contains = TRUE, max_angle_diff = NULL,
+                      crs = geo_select_aeq(rnet_x), ...) {
   if (is.null(dist_subset)) {
-    dist_subset = dist + 1
+    dist_subset <- dist + 1
   }
   if (subset_x) {
-    rnet_x = rnet_subset(rnet_x, rnet_y, dist = dist_subset, ...)
+    rnet_x <- rnet_subset(rnet_x, rnet_y, dist = dist_subset, ...)
   }
-  if(!is.null(max_angle_diff)) {
-    rnet_x$angle_x = line_bearing(rnet_x, bidirectional = TRUE)
-    contains = FALSE
+  if (!is.null(max_angle_diff)) {
+    rnet_x$angle_x <- line_bearing(rnet_x, bidirectional = TRUE)
+    contains <- FALSE
   }
-  rnet_x_buffer = geo_buffer(rnet_x, dist = dist, nQuadSegs = 2, endCapStyle = endCapStyle, crs = crs)
+  rnet_x_buffer <- geo_buffer(rnet_x, dist = dist, nQuadSegs = 2, endCapStyle = endCapStyle, crs = crs)
   if (segment_length > 0) {
-    browser()
-    rnet_y = line_segment(rnet_y, segment_length = segment_length)
+    rnet_y <- line_segment(rnet_y, segment_length = segment_length)
   }
-  if(!is.null(max_angle_diff)) {
-    rnet_y$angle_y = line_bearing(rnet_y, bidirectional = TRUE)
+  if (!is.null(max_angle_diff)) {
+    rnet_y$angle_y <- line_bearing(rnet_y, bidirectional = TRUE)
   }
   if (length_y) {
-    rnet_y$length_y = as.numeric(sf::st_length(rnet_y))
+    rnet_y$length_y <- as.numeric(sf::st_length(rnet_y))
   }
   if (contains) {
-    rnetj = sf::st_join(rnet_x_buffer[key_column], rnet_y, join = sf::st_contains)
+    rnetj <- sf::st_join(rnet_x_buffer[key_column], rnet_y, join = sf::st_contains)
     # # For debugging:
     # library(tmap)
     # tmap_mode("view")
     # tm_shape(rnet_y) + tm_lines(lwd = 3) + qtm(rnetj) + qtm(rnet_x) +
     #   qtm(osm_net_example)
   } else {
-    rnet_y_centroids = sf::st_centroid(rnet_y)
-    rnetj = sf::st_join(rnet_x_buffer[c(names(rnet_x)[1], "angle_x")], rnet_y_centroids)
+    rnet_y_centroids <- sf::st_centroid(rnet_y)
+    rnetj <- sf::st_join(rnet_x_buffer[c(names(rnet_x)[1], "angle_x")], rnet_y_centroids)
   }
   if (!is.null(max_angle_diff)) {
-    rnetj$angle_diff = rnetj$angle_y - rnetj$angle_x
-    rnetj = rnetj[abs(rnetj$angle_diff) < max_angle_diff, ]
+    rnetj$angle_diff <- rnetj$angle_y - rnetj$angle_x
+    rnetj <- rnetj[abs(rnetj$angle_diff) < max_angle_diff, ]
   }
   rnetj
 }
@@ -136,47 +135,36 @@ rnet_join = function(rnet_x, rnet_y, dist = 5, length_y = TRUE, key_column = 1,
 #' @param rm_disconnected Remove ways that are
 #' @export
 #' @examples
-#' rnet_x = osm_net_example[1]
-#' rnet_y = route_network_small["flow"]
+#' rnet_x <- osm_net_example[1]
+#' rnet_y <- route_network_small["flow"]
 #' plot(rnet_x$geometry, lwd = 5)
 #' plot(rnet_y$geometry, add = TRUE, col = "red", lwd = 3)
-#' rnet_x_subset = rnet_subset(rnet_x, rnet_y)
+#' rnet_x_subset <- rnet_subset(rnet_x, rnet_y)
 #' plot(rnet_x_subset, add = TRUE, col = "blue")
-rnet_subset = function(rnet_x, rnet_y, dist = 10, crop = TRUE, min_length = 20, rm_disconnected = TRUE) {
-  # browser()
-  rnet_x_original = data.frame(
+rnet_subset <- function(rnet_x, rnet_y, dist = 10, crop = TRUE, min_length = 20, rm_disconnected = TRUE) {
+  rnet_x_original <- data.frame(
     id = rnet_x[[1]],
     length_original = as.numeric(sf::st_length(rnet_x))
-    )
-  names(rnet_x_original)[1] = names(rnet_x)[1]
-  rnet_y_union = sf::st_union(rnet_y)
-  rnet_y_buffer = stplanr::geo_buffer(rnet_y_union, dist = dist, nQuadSegs = 2)
-  if(crop) {
-    rnet_x = sf::st_intersection(rnet_x, rnet_y_buffer)
-    rnet_x = line_cast(rnet_x)
+  )
+  names(rnet_x_original)[1] <- names(rnet_x)[1]
+  rnet_y_union <- sf::st_union(rnet_y)
+  rnet_y_buffer <- stplanr::geo_buffer(rnet_y_union, dist = dist, nQuadSegs = 2)
+  if (crop) {
+    rnet_x <- sf::st_intersection(rnet_x, rnet_y_buffer)
+    rnet_x <- line_cast(rnet_x)
   } else {
-    rnet_x = rnet_x[rnet_y_buffer, , op = sf::st_within]
+    rnet_x <- rnet_x[rnet_y_buffer, , op = sf::st_within]
   }
-  if(min_length > 0) {
-    rnet_x$length_new = as.numeric(sf::st_length(rnet_x))
-    rnet_x_joined = dplyr::left_join(rnet_x, rnet_x_original)
-    sel_short_remove = rnet_x_joined$length_new < min_length
-    sel_changed_remove = rnet_x_joined$length_new < rnet_x_joined$length_original
-    sel_remove = sel_short_remove & sel_changed_remove
-
-    # browser()
-    # # Testing:
-    # # ids_to_keep = rnet_x_joined[[1]][!sel_remove]
-    # rnet_x_joined[sel_remove, ]
-    # plot(rnet_x_joined$geometry[sel_remove])
-    # plot(rnet_x_joined$geometry[!sel_remove])
-    # rnet_x_original_full = rnet_x
-    # rnet_x = rnet_x[rnet_x[[1]] %in% ids_to_keep, ]
-
-    rnet_x = rnet_x_joined[!sel_remove, ]
+  if (min_length > 0) {
+    rnet_x$length_new <- as.numeric(sf::st_length(rnet_x))
+    rnet_x_joined <- dplyr::left_join(rnet_x, rnet_x_original)
+    sel_short_remove <- rnet_x_joined$length_new < min_length
+    sel_changed_remove <- rnet_x_joined$length_new < rnet_x_joined$length_original
+    sel_remove <- sel_short_remove & sel_changed_remove
+    rnet_x <- rnet_x_joined[!sel_remove, ]
   }
-  if(rm_disconnected) {
-    rnet_x = rnet_connected(rnet_x)
+  if (rm_disconnected) {
+    rnet_x <- rnet_connected(rnet_x)
   }
   rnet_x
 }
@@ -187,7 +175,7 @@ rnet_subset = function(rnet_x, rnet_y, dist = 10, crop = TRUE, min_length = 20, 
 #'
 #' @param x Linestring object
 #' @export
-line_cast = function(x) {
+line_cast <- function(x) {
   sf::st_cast(sf::st_cast(x, "MULTILINESTRING"), "LINESTRING")
 }
 
@@ -202,15 +190,16 @@ line_cast = function(x) {
 #' @export
 #' @examples
 #' # The source object:
-#' rnet_y = route_network_small["flow"]
+#' rnet_y <- route_network_small["flow"]
 #' # The target object
-#' rnet_x = rnet_subset(osm_net_example[1], rnet_y)
+#' rnet_x <- rnet_subset(osm_net_example[1], rnet_y)
 #' plot(rnet_x$geometry, lwd = 5)
 #' plot(rnet_y$geometry, add = TRUE, col = "red", lwd = 2)
-#' rnet_y$quietness = rnorm(nrow(rnet_y))
-#' funs = list(flow = sum, quietness = mean)
-#' rnet_merged = rnet_merge(rnet_x[1], rnet_y[c("flow", "quietness")],
-#'                          dist = 9, segment_length = 20, funs = funs)
+#' rnet_y$quietness <- rnorm(nrow(rnet_y))
+#' funs <- list(flow = sum, quietness = mean)
+#' rnet_merged <- rnet_merge(rnet_x[1], rnet_y[c("flow", "quietness")],
+#'   dist = 9, segment_length = 20, funs = funs
+#' )
 #' plot(rnet_y$geometry, lwd = 5, col = "lightgrey")
 #' plot(rnet_merged["flow"], add = TRUE, lwd = 2)
 #'
@@ -231,53 +220,53 @@ line_cast = function(x) {
 #' @return An sf object with the same geometry as `rnet_x`
 rnet_merge <- function(rnet_x, rnet_y, dist = 5, funs = NULL, sum_flows = TRUE, crs = geo_select_aeq(rnet_x), ...) {
   if (is.null(funs)) {
-    print('funs is NULL')
-    funs = list()
+    print("funs is NULL")
+    funs <- list()
     for (col in names(rnet_y)) {
       if (is.numeric(rnet_y[[col]])) {
-        funs[[col]] = sum
+        funs[[col]] <- sum
       }
     }
   }
-  sum_cols = sapply(funs, function(f) identical(f, sum))
-  sum_cols = names(funs)[which(sum_cols)]
-  rnetj = rnet_join(rnet_x, rnet_y, dist = dist, crs = crs, ...)
+  sum_cols <- sapply(funs, function(f) identical(f, sum))
+  sum_cols <- names(funs)[which(sum_cols)]
+  rnetj <- rnet_join(rnet_x, rnet_y, dist = dist, crs = crs, ...)
   names(rnetj)
-  rnetj_df = sf::st_drop_geometry(rnetj)
+  rnetj_df <- sf::st_drop_geometry(rnetj)
   # Apply functions to columns with lapply:
   res_list <- lapply(seq_along(funs), function(i) {
     # i = 1
-    nm = names(funs[i])
-    fn = funs[[i]]
+    nm <- names(funs[i])
+    fn <- funs[[i]]
 
     if (identical(fn, sum) && sum_flows) {
-      res = rnetj_df %>%
+      res <- rnetj_df %>%
         dplyr::group_by_at(1) %>%
         dplyr::summarise(dplyr::across(dplyr::all_of(nm), function(x) sum(x * length_y)))
     } else {
-      res = rnetj_df %>%
+      res <- rnetj_df %>%
         dplyr::group_by_at(1) %>%
         dplyr::summarise(dplyr::across(dplyr::all_of(nm), fn))
     }
-    names(res)[2] = nm
-    if(i > 1) {
-      res = res[-1]
+    names(res)[2] <- nm
+    if (i > 1) {
+      res <- res[-1]
     }
     res
   })
-  res_df = dplyr::bind_cols(res_list)
+  res_df <- dplyr::bind_cols(res_list)
 
-  res_sf = dplyr::left_join(rnet_x, res_df)
+  res_sf <- dplyr::left_join(rnet_x, res_df)
   if (sum_flows) {
-    res_sf$length_x = as.numeric(sf::st_length(res_sf))
-    for(i in sum_cols) {
+    res_sf$length_x <- as.numeric(sf::st_length(res_sf))
+    for (i in sum_cols) {
       # TODO: deduplicate
-      length_y = as.numeric(sf::st_length(rnet_y))
+      length_y <- as.numeric(sf::st_length(rnet_y))
       # i = sum_cols[1]
-      res_sf[[i]] = res_sf[[i]] / res_sf$length_x
-      over_estimate = sum(res_sf[[i]] * res_sf$length_x, na.rm = TRUE) /
+      res_sf[[i]] <- res_sf[[i]] / res_sf$length_x
+      over_estimate <- sum(res_sf[[i]] * res_sf$length_x, na.rm = TRUE) /
         sum(rnet_y[[i]] * length_y, na.rm = TRUE)
-      res_sf[[i]] = res_sf[[i]] / over_estimate
+      res_sf[[i]] <- res_sf[[i]] / over_estimate
     }
   }
   res_sf
