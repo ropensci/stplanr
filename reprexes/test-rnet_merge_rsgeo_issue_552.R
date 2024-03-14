3371d91b30ab8ee5fa9c0ebcd148d3aca2a5a5b2
robinlovelace <rob00x@gmail.com>
2024-03-14 14:57:08 +0000

    Demonstrate issue with #554

--- reprexes/test-rnet_merge_rsgeo_issue_552.R
+++ reprexes/test-rnet_merge_rsgeo_issue_552.R
@@ -8,14 +8,18 @@
 # l <- sf::read_sf("test_line.geojson")
 # n_segments <- jsonlite::read_json("n_segments.json", simplifyVector = TRUE)
 
-l <- sf::read_sf("https://github.com/ropensci/stplanr/releases/download/v1.1.2/test_line.geojson")
-n_segments <- jsonlite::read_json("https://github.com/ropensci/stplanr/releases/download/v1.1.2/n_segments.json")
 
-res_rsgeo <- rsgeo::line_segmentize(l$geometry, n_segments)
-length(res_rsgeo)
+l <- sf::read_sf("https://github.com/ropensci/stplanr/releases/download/v1.1.2/test_line.geojson")
+n_segments <- jsonlite::read_json("https://github.com/ropensci/stplanr/releases/download/v1.1.2/n_segments.json", simplifyVector = TRUE)
+
+
+# extract geometry and convert to rsgeo
+geo <- rsgeo::as_rsgeo(sf::st_geometry(l))
+
+# segmentize the line strings
+res_rsgeo <- rsgeo::line_segmentize(geo, n_segments)
 
 # make them into sfc_LINESTRING
 res <- sf::st_cast(sf::st_as_sfc(res_rsgeo), "LINESTRING")
-
 length(res)
 sum(n_segments)
